c5b27c70e03408b106d730112ea6720a42a3ff56
leandro antunes <leandro.tune@hotmail.com>
2022-10-03 18:28:39 -0300

    Componente: CardPost - concluído

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { CardPost } from '../../components/CardPost'
 import { Profile } from '../../components/Profile'
 import { Search } from '../../components/Search'
 
@@ -7,6 +8,14 @@ export function Home() {
       <Profile />
       <main className="max-w-[54rem] mt-[4.5rem] mx-auto">
         <Search />
+        <ul className="mb-[234px] mt-12 grid grid-cols-2 gap-8">
+          <CardPost />
+          <CardPost />
+          <CardPost />
+          <CardPost />
+          <CardPost />
+          <CardPost />
+        </ul>
       </main>
     </div>
   )
