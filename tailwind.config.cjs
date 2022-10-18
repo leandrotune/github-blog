14a9f2e7da70d317e99a433f12d8a1d6b3bd1989
leandro antunes <leandro.tune@hotmail.com>
2022-10-18 19:36:08 -0300

    algumas alterações

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,23 +2,41 @@
 module.exports = {
   content: ['./src/**/*.tsx', './index.html'],
   theme: {
+    fontSize: {
+      xs: 12,
+      sm: 14,
+      md: 16,
+      lg: 18,
+      xl: 20,
+      '2xl': 24,
+    },
     colors: {
-      blue: '#3294F8',
-      'base-title': '#E7EDF4',
-      'base-subtitle': '#C4D4E3',
-      'base-text': '#AFC2D4',
-      'base-span': '#7B96B2',
-      'base-label': '#3A536B',
-      'base-border': '#1C2F41',
-      'base-post': '#112131',
-      'base-profile': '#0B1B2B',
-      'base-header': '#0C2036',
-      'base-background': '#071422',
-      'base-input': '#040F1A',
+      transparent: 'transparent',
+
+      blue: {
+        500: '#3294F8',
+      },
+
+      gray: {
+        100: '#E7EDF4',
+        200: '#C4D4E3',
+        300: '#AFC2D4',
+        400: '#7B96B2',
+        500: '#3A536B',
+        600: '#1C2F41',
+        700: '#112131',
+        800: '#0B1B2B',
+        900: '#0C2036',
+      },
+
+      black: {
+        800: '#071422',
+        900: '#040F1A',
+      },
     },
     extend: {
       fontFamily: {
-        Nubito: ['Nunito', 'sans-serif'],
+        sans: 'Nunito, sans-serif',
       },
     },
   },
